1d86caf379f3296d75e59ae799645bad7017a9a6
Urs Graf <urs.graf@ntb.ch>
2019-06-28 11:40:10 +0200

    Add support for multiple device files

--- src/ch/ntb/inf/openOCDInterface/OpenOCD.java
+++ src/ch/ntb/inf/openOCDInterface/OpenOCD.java
@@ -1,6 +1,8 @@
 package ch.ntb.inf.openOCDInterface;
 
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Map;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -257,21 +259,49 @@ public class OpenOCD extends TargetConnection {
 	public void downloadImageFile(String filename) throws TargetConnectionException {
 		try {
 			while (in.available() > 0) in.read();	// empty buffer
-			String name = filename;
-			name = name.replace('\\', '/');
 			out.write((("halt\r\n").getBytes()));
-			out.write((("load_image " + name + " \r\n").getBytes()));
-			if (dbg) StdStreams.vrb.println("[TARGET] loading: " + name);
-			StringBuffer buf = new StringBuffer();
-			while (true) {
-				int n = in.available();
-				if (n <= 0) Thread.sleep(100);
-				int c = in.read();
-				if (c < 0) throw new TargetConnectionException("target not answering");
-				buf.append((char)c);
-				if (buf.indexOf("downloaded") > 0) {
-					waitForNL(1);
-					break;
+			
+            /*Init PS*/
+            StdStreams.log.print("Initializing PS");
+            out.write(("initPS\r\n").getBytes());
+
+            StringBuffer buf = new StringBuffer();
+            while (true) {
+                   int n = in.available();
+                   if (n <= 0) {
+                          Thread.sleep(100);
+                          StdStreams.log.print(".");
+                   }
+                   int c = in.read();
+                   if (c < 0) throw new TargetConnectionException("target not answering");
+                   
+                   buf.append((char)c);
+                   if (buf.indexOf("initializing") > 0) {
+                          waitForNL(1);
+                          StdStreams.log.println(" initPS complete");
+                          break;
+                   }
+            }
+
+			ArrayList<Map.Entry<String,Integer>> files = Configuration.getImgFile();
+			if (files.isEmpty()) StdStreams.err.println("no image files available");
+			for (Map.Entry<String,Integer> file : files) {
+				String name = file.getKey();
+				name = name.replace('\\', '/');
+	            StdStreams.log.println("Downloading " + name);
+				out.write((("load_image " + name + " " + file.getValue() + " \r\n").getBytes()));
+				if (dbg) StdStreams.vrb.println("[TARGET] loading: " + name);
+				buf = new StringBuffer();
+				while (true) {
+					int n = in.available();
+					if (n <= 0) Thread.sleep(100);
+					int c = in.read();
+					if (c < 0) throw new TargetConnectionException("target not answering");
+					buf.append((char)c);
+					if (buf.indexOf("downloaded") > 0) {
+						waitForNL(1);
+						break;
+					}
 				}
 			}
 		} catch (Exception e) {
